a0ead63b5c71cd1b71366c75f0e608ac721824d0
parkdl <ehddlf2395@gmail.com>
2022-01-05 17:49:10 +0900

    filter function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
 const frame = 'section';
 const box = 'article';
 const speed = '0.5s';
+const btns = document.querySelectorAll('main .filter li');
 let grid;
+const activeClass = 'on';
+
+function filter(btns) {
+    for (let btn of btns) {
+        btn.addEventListener('click', e => {
+            e.preventDefault();
+            console.log(e.currentTarget);
+
+            const sort = e.currentTarget
+                .querySelector('a')
+                .getAttribute('href');
+
+            grid.arrange({
+                filter: sort,
+            });
+
+            for (let btn of btns) {
+                btn.classList.remove(activeClass);
+            }
+
+            e.currentTarget.classList.add(activeClass);
+        });
+    }
+}
 
 function init() {
     grid = new Isotope(frame, {
@@ -13,4 +38,5 @@ function init() {
 
 window.addEventListener('load', () => {
     init();
+    filter(btns);
 });
